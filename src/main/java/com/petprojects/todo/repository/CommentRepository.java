d1b44da8a85a64ec4700fe3db4b5e5426c42f98c
Ilya <pavlov.ilya.y@gmail.com>
2024-01-14 19:34:25 +0300

    CommentRepository: changed return value of findByTask_Id to List

--- src/main/java/com/petprojects/todo/repository/CommentRepository.java
+++ src/main/java/com/petprojects/todo/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.petprojects.todo.repository;
 
 import com.petprojects.todo.model.Comment;
 
+import java.util.List;
+
 public interface CommentRepository extends GenericRepository<Comment> {
-    Comment findByTask_Id(int id);
+    List<Comment> findByTask_Id(int id);
 }
