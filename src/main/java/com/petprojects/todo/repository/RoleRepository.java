d058a88efa27650c1492c747e647dc38d0786b26
Ilya <pavlov.ilya.y@gmail.com>
2024-01-14 19:39:11 +0300

    Removed redundant 'spaces' from repositories

--- src/main/java/com/petprojects/todo/repository/RoleRepository.java
+++ src/main/java/com/petprojects/todo/repository/RoleRepository.java
@@ -3,6 +3,5 @@ package com.petprojects.todo.repository;
 import com.petprojects.todo.model.Role;
 
 public interface RoleRepository extends GenericRepository<Role>{
-
     Role findByName(String name);
 }
--- src/main/java/com/petprojects/todo/repository/UserRepository.java
+++ src/main/java/com/petprojects/todo/repository/UserRepository.java
@@ -4,7 +4,6 @@ package com.petprojects.todo.repository;
 import com.petprojects.todo.model.User;
 
 public interface UserRepository extends GenericRepository<User>{
-
     User findUserByLogin(String login);
     User findUserByEmail(String email);
 }
