021f20baf127e84ba8a82e73b8e4725299f1c325
Ilya <pavlov.ilya.y@gmail.com>
2024-01-14 20:11:44 +0300

    Added property 'comments' to the Task entity, and therefore  the property 'task' in the Comment entity and method 'findAllByTask_Id' from CommentRepository were removed

--- src/main/java/com/petprojects/todo/repository/CommentRepository.java
+++ src/main/java/com/petprojects/todo/repository/CommentRepository.java
@@ -5,5 +5,4 @@ import com.petprojects.todo.model.Comment;
 import java.util.List;
 
 public interface CommentRepository extends GenericRepository<Comment> {
-    List<Comment> findByTask_Id(int id);
 }
